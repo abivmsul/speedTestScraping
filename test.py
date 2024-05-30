31c5326dbe24c4fc6c5e81aba7f8198d0923cc2b
abivmsu <abivmsul24@gmail.com>
2024-05-30 16:48:11 +0300

    add the getspeed function to get the download and upload rate

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class SpeedTestBot:
@@ -9,5 +12,21 @@ class SpeedTestBot:
         self.up = 0
         self.down = 0
 
+    def getspeed(self):
+        self.driver.get('https://www.speedtest.net/')
+        time.sleep(15)
+        go = self.driver.find_element(By.CSS_SELECTOR, '.start-button a')
+        go.click()
+        time.sleep(60)
+
+        self.down = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div['
+                                                       '3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+        self.up = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div['
+                                                     '3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        print("Down :" + str(self.down))
+        print("Up :" + str(self.up))
+        self.driver.quit()
+
 
 Get = SpeedTestBot()
+Get.getspeed()
